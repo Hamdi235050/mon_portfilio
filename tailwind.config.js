e86f1de511d52e689bfa6ecd8e2688b82e4e8aa9
hamdi.drine <hamdi.drine@continuousnet.com>
2024-01-06 20:25:28 +0100

    config tailwindcc && create some components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "bg-blcak" : "#000000" ,
+         "bg-white": "#FFFFFF"
+      }
+    },
   },
   plugins: [],
 }
